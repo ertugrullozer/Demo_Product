ddf4f8a9ffb72b4708e0eb38ab6bf149601641b0
ertugrullozer <ertugrulozer19944@gmail.com>
2024-03-24 22:12:57 +0300

    add-migration mig1 Migration İşlemi Yapıldı VT Oluşturuldu

---
+++ DataAccessLayer/Contcrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Contcrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-62HEUCO\\SQLEXPRESS;databse=DbNewCore1;" +
+                "integrated security=true;TrustServerCertificate=true;");
+        }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
